c38a9fd949af060b79bbaba08072431af212598d
chris <chrisshields88@gmail.com>
2018-03-09 06:22:24 -0500

    Initial Commit
    Initial Commit of project files, also added a header.

---
+++ portfolio/src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <b-navbar toggleable="md" type="dark" variant="dark">
+
+  <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+
+  <b-navbar-brand href="#">NavBar</b-navbar-brand>
+
+  <b-collapse is-nav id="nav_collapse">
+
+    <b-navbar-nav>
+      <b-nav-item href="#">Link</b-nav-item>
+      <b-nav-item href="#" disabled>Disabled</b-nav-item>
+    </b-navbar-nav>
+
+    <!-- Right aligned nav items -->
+    <b-navbar-nav class="ml-auto">
+
+      
+    <b-navbar-nav>
+      <b-nav-item href="#">Home</b-nav-item>
+      <b-nav-item href="#">About</b-nav-item>
+      <b-nav-item href="#">Portfolio</b-nav-item>
+      <b-nav-item href="#">Contact</b-nav-item>
+    </b-navbar-nav>
+    </b-navbar-nav>
+  </b-collapse>
+</b-navbar>
+
+<!-- navbar-1.vue -->
+</template>
