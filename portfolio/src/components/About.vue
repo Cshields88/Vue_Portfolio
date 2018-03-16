d33694f0e7dfb936c8d5a9b5c223108c56ccbb3b
chris <chrisshields88@gmail.com>
2018-03-16 06:55:29 -0400

    update
    finished skills section

--- portfolio/src/components/About.vue
+++ portfolio/src/components/About.vue
@@ -3,24 +3,37 @@
     
 <h4>Skills</h4>
 
-    <b-progress-bar :value="100">HTML</b-progress-bar>
-    <b-progress-bar :value="80">CSS</b-progress-bar>
-    <b-progress-bar :value="70">Javascript</b-progress-bar>
-    <b-progress-bar :value="50" >Vue.JS</b-progress-bar>
-     <b-progress class="mt-1" :max="max" show-value>
-      <b-progress-bar :value="60" variant="success">Vue.JS</b-progress-bar>
-      <b-progress-bar :value="40" variant="warning">Knockout.JS</b-progress-bar>
-      <b-progress-bar :value="" variant="danger"></b-progress-bar>
+    <b-progress-bar class="purple" :value="100">HTML</b-progress-bar>
+    <b-progress-bar class="purple" :value="80">CSS</b-progress-bar>
+    <b-progress-bar class="purple" :value="70">Javascript</b-progress-bar>
+    <b-progress-bar class="purple" :value="50" >Vue.JS</b-progress-bar>
+  
+      <b-progress class="mt-1" :max='max' show-value>
+      <b-progress-bar :value="70" variant="success">Vue.JS</b-progress-bar>
+      <b-progress-bar :value="30" variant="warning">Knockout.JS</b-progress-bar>
+      <b-progress-bar :value="20*(1.5/10)" variant="danger">Angular.JS</b-progress-bar>
     </b-progress>
  
 </div>
 </template>
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      max: 100
+    };
+  }
+};
 </script>
 
 <style>
 .progress-bar {
+  /* margin: 10px; */
+  /* border: black 1px solid;
+  border-radius: 5px;
+  background-color: purple; */
+}
+.purple {
   margin: 10px;
   border: black 1px solid;
   border-radius: 5px;
